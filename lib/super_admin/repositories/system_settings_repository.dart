aff58c6f60b2d462aca8d673b675b88715e7b513
Muchammad Ridho <muchammadridho6901@gmail.com>
2026-07-25 00:19:25 +0700

    test(super-admin): integration testing

---
+++ lib/super_admin/repositories/system_settings_repository.dart
@@ -0,0 +1,21 @@
+import 'base_repository.dart';
+
+class SystemSettingsRepository extends BaseRepository<Map<String, dynamic>> {
+  SystemSettingsRepository() : super('system_settings');
+
+  @override
+  Map<String, dynamic> fromJson(Map<String, dynamic> json) => json;
+
+  @override
+  Map<String, dynamic> toJson(Map<String, dynamic> item) => item;
+
+  
+
+  
+
+  
+
+  
+
+  
+}
---
+++ lib/super_admin/repositories/transaction_repository.dart
@@ -0,0 +1,17 @@
+﻿import 'base_repository.dart';
+import '../modules/transaction_management/models/transaction_models.dart';
+
+class TransactionRepository extends BaseRepository<TransactionRecord> {
+  TransactionRepository() : super('orders');
+
+  @override
+  TransactionRecord fromJson(Map<String, dynamic> json) => TransactionRecord.fromJson(json);
+
+  @override
+  Map<String, dynamic> toJson(TransactionRecord item) => item.toJson();
+
+  Future<TransactionRecord> updateStatus(String id, String status) async {
+    final data = await supabase.update('orders', id, {'status': status, 'updated_at': DateTime.now().toIso8601String()});
+    return fromJson(data);
+  }
+}
